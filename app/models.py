714939cd3cf7a92513e7564395363fce20e1d0d4
Melissa Skevington <mskeving@gmail.com>
2017-07-12 21:02:20 -0700

    models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,24 @@
 from app import db
 
-class Example(db.Model):
-    __tablename__ = "example"
+class User(db.Model):
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    avatar_url = db.Column(db.Text())
+    email_address = db.Column(db.Text())
+    first_name = db.Column(db.Text())
+    last_name = db.Column(db.Text())
+
+
+class Message(db.Model):
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Text())
+    body = db.Column(db.Text())
+    message_id = db.Column(db.Text(), unique=True)
+    pruned = db.Column(db.Text())  # without quoted replies
+    send_time_unix = db.Column(db.Text())  # unix
+    subject = db.Column(db.Text())
+    thread_id = db.Column(db.Text())
+    type = db.Column(db.Text())  # "email" or "chat"
