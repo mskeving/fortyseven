cd117eac25ecd479bccf6826064e0804046d68a3
Melissa Skevington <mskeving@gmail.com>
2017-07-13 22:04:37 -0700

    cleanup framework files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-# this needs to be here to run scripts.
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/NAME_OF_DB"
-
 app = Flask(__name__, template_folder='static/templates')
 app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
-
 
 def create_app(config):
     app.config.from_object('config.flask.' + config)
