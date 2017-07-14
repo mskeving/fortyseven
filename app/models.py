9dc2ab4b15f04cb17f2283c99367b0cde9501271
Melissa Skevington <mskeving@gmail.com>
2017-07-13 21:43:34 -0700

    updated models

--- app/models.py
+++ app/models.py
@@ -16,9 +16,9 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text())
     body = db.Column(db.Text())
+    label = db.Column(db.Text())  # "chats"
     message_id = db.Column(db.Text(), unique=True)
-    pruned = db.Column(db.Text())  # without quoted replies
-    send_time_unix = db.Column(db.Text())  # unix
+    pruned = db.Column(db.Text())
+    send_time_unix = db.Column(db.Text())
     subject = db.Column(db.Text())
     thread_id = db.Column(db.Text())
-    type = db.Column(db.Text())  # "email" or "chat"
