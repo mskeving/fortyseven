c1b953b6994a7cbcf9950ecc315c0fe594b93505
Phil Rha <philrha@gmail.com>
2017-07-16 19:14:14 -0700

    add redux scaffolding

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/reset.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Routes />,
+import './styles/reset.css';
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
+import Links from './components/Links';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route path="/links" component={Links} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
